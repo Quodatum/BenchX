e9dc0aed007553def35eae541ecf4b90cd94de21
andy <bunce.andy@gmail.com>
2014-05-08 23:48:46 +0100

    hotkeys,logmsg

--- src/static/benchmark/app.js
+++ src/static/benchmark/app.js
@@ -1,6 +1,6 @@
 var App = angular.module(
 		'benchapp',
-		[ 'ngRoute', 'ngResource', 'ui.bootstrap',
+		[ 'ngRoute', 'ngResource', 'ui.bootstrap', 'cfp.hotkeys',
 				'services.httpRequestTracker' ])
 
 .constant("apiRoot", "../../benchmark/")
@@ -18,9 +18,9 @@ var App = angular.module(
 	}).when('/about', {
 		templateUrl : '/static/benchmark/templates/about.xml'
 	}).when('/library', {
-		templateUrl : '/static/benchmark/templates/library.xml'		
+		templateUrl : '/static/benchmark/templates/library.xml'
 	}).when('/xqdoc', {
-		templateUrl : '/static/benchmark/templates/xqdoc.xml'		
+		templateUrl : '/static/benchmark/templates/xqdoc.xml'
 	}).when('/404', {
 		templateUrl : '/static/benchmark/templates/404.xml',
 		controller : "queriesController"
@@ -29,29 +29,43 @@ var App = angular.module(
 	});
 } ])
 
-.controller(
-		'rootController',
-		[ '$rootScope', 'httpRequestTracker', 'api', 'queries', '$modal',
-				function($rootScope, httpRequestTracker, api, queries, $modal) {
-					function updateStatus(data) {
-						console.log("update status:", data)
-						$rootScope.state = data.state;
-					}
-					;
-					$rootScope.$on('$routeChangeError', 
-							function(event, cur, prev, rejection) {
-						alert("bad thing");
+.run(
+		[
+				'$rootScope',
+				'queries',
+				'httpRequestTracker',
+				function($rootScope, queries, httpRequestTracker) {
+					$rootScope.hasPendingRequests = function() {
+						return httpRequestTracker.hasPendingRequests();
+					};
+					$rootScope.$on('$routeChangeError', function(event, cur,
+							prev, rejection) {
+						alert("routeChangeError @TODO");
 					});
-					$rootScope.queries = [];
-					$rootScope.logmsg="Welcome to Benchmark";
+
+					$rootScope.logmsg = "Welcome to Benchmark";
+					queries.getData().then(function(data) {
+						$rootScope.queries = data;
+					});
 					$rootScope.queue = async.queue(function(index, callback) {
-						console.log('start ' + index);
+						$rootScope.logmsg = 'starting ' + index;
 						$rootScope.execute(index).then(function(res) {
 							// Dig into the responde to get the relevant data
-							console.log('done ' + index, res);
+							$rootScope.logmsg = 'end of ' + index;
 							callback();
 						});
 					}, 1);
+				} ])
+
+.controller(
+		'rootController',
+		[ '$rootScope', 'api', 'queries', '$modal',
+				function($rootScope, api, queries, $modal) {
+					function updateStatus(data) {
+						console.log("update status:", data);
+						$rootScope.state = data.state;
+					}
+					;
 
 					// run query with index
 					$rootScope.execute = function(index) {
@@ -83,16 +97,18 @@ var App = angular.module(
 						});
 					};
 					$rootScope.saveAs = function() {
-						 var heads=["name","mode","factor","runtime"];
-						 var txt=[heads.join(",")];
-						 angular.forEach($rootScope.queries, function(v) {	
-							 angular.forEach(v.runs, function(r) {
-								 line=[v.name,r.mode,r.factor,r.runtime];
-								 txt.push(line.join(","));
+						var heads = [ "name", "mode", "factor", "runtime" ];
+						var txt = [ heads.join(",") ];
+						angular.forEach($rootScope.queries, function(v) {
+							angular.forEach(v.runs, function(r) {
+								line = [ v.name, r.mode, r.factor, r.runtime ];
+								txt.push(line.join(","));
 							});
-						 });
-						 var blob= new Blob([txt.join("\n")], {type : 'text/csv'})
-						 saveAs(blob,"results.csv");
+						});
+						var blob = new Blob([ txt.join("\n") ], {
+							type : 'text/csv'
+						});
+						saveAs(blob, "results.csv");
 					};
 
 					$rootScope.xmlgen = function() {
@@ -110,67 +126,68 @@ var App = angular.module(
 
 					api.status().success(updateStatus);
 
-					$rootScope.hasPendingRequests = function() {
-						return httpRequestTracker.hasPendingRequests();
-					};
 				} ])
 
 .controller(
 		'queriesController',
-		[ "$scope", "$rootScope", "queries",
-				function($scope, $rootScope, queries) {
+		[
+				"$scope",
+				"$rootScope",
+				"queries",
+				"hotkeys",
+				function($scope, $rootScope, queries, hotkeys) {
 					console.log("queries");
-					queries.getData().then(function(data) {
-						$rootScope.queries = data;
-					});
+					hotkeys.add("T", "toggles mode between file and database",
+							$rootScope.toggleMode);
+					hotkeys.add("X", "run all queries", $rootScope.executeAll);
+
 				} ])
 
 .controller('envController', [ "$scope", "api", function($scope, api) {
 	api.environment().then(function(d) {
 		$scope.envs = d.env;
 	});
-} ])
-.filter('readablizeBytes', function() {
-    return function (bytes) {
-        var s = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'];
-        var e = Math.floor(Math.log(bytes) / Math.log(1024));
-        return (bytes / Math.pow(1024, Math.floor(e))).toFixed(2) + " " + s[e]; }
-})
-.filter('factorBytes', function() {
-  return function(input) {
-    return (116.47106113642*input -0.00057972324877298)*1000000;
-  }
-})
-.factory('api', [ '$http', '$resource','apiRoot', function($http,$resource, apiRoot) {
-	return {
-
-		status : function() {
-			return $http({
-				method : 'GET',
-				url : apiRoot + 'status'
-			});
-		},
-		xmlgen : function(factor) {
-			return $http({
-				method : 'POST',
-				url : apiRoot + 'xmlgen',
-				params : {
-					factor : factor
-				}
-			});
-		},
-		toggleMode : function() {
-			return $http({
-				method : 'POST',
-				url : apiRoot + 'manage'
-			});
-		},
-		environment : function() {
-			return $resource(apiRoot + 'environment').get().$promise;
-				
-		}
+} ]).filter('readablizeBytes', function() {
+	return function(bytes) {
+		var s = [ 'bytes', 'kB', 'MB', 'GB', 'TB', 'PB' ];
+		var e = Math.floor(Math.log(bytes) / Math.log(1024));
+		return (bytes / Math.pow(1024, Math.floor(e))).toFixed(2) + " " + s[e];
 	}
-} ])
+}).filter('factorBytes', function() {
+	return function(input) {
+		return (116.47106113642 * input - 0.00057972324877298) * 1000000;
+	}
+}).factory('api',
+		[ '$http', '$resource', 'apiRoot', function($http, $resource, apiRoot) {
+			return {
+
+				status : function() {
+					return $http({
+						method : 'GET',
+						url : apiRoot + 'status'
+					});
+				},
+				xmlgen : function(factor) {
+					return $http({
+						method : 'POST',
+						url : apiRoot + 'xmlgen',
+						params : {
+							factor : factor
+						}
+					});
+				},
+				toggleMode : function() {
+					return $http({
+						method : 'POST',
+						url : apiRoot + 'manage'
+					});
+				},
+				environment : function() {
+					return $resource(apiRoot + 'environment').get().$promise;
+
+				}
+			}
+		} ])
 
 .factory('queries', [ '$http', '$q', 'apiRoot', function($http, $q, apiRoot) {
 	var q = [];
